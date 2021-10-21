b66e49a228968f0e9a4afc92b5c30f8a3b08592c
picode98 <saaman1377@gmail.com>
2021-10-21 01:00:46 -0400

    Add missing files not included in previous commits

---
+++ QuickOpen/ApplicationInfo.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define QUICKOPEN_PRODUCT_NAME "QuickOpen"
+#define QUICKOPEN_VERSION_STR "0.1.0.0"
+#define QUICKOPEN_DEVELOPER "Saaman Khalilollahi (picode98)"
+#define QUICKOPEN_REPO_URL "https://github.com/picode98/QuickOpen"
+#define QUICKOPEN_SHORT_DESC "QuickOpen: Easily share content with others on your LAN"
+#define QUICKOPEN_LICENSE "Licensing TBD"
