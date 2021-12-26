3f2d3db93a2788ea0695685b21ed17b56e06aa54
picode98 <saaman1377@gmail.com>
2021-12-25 22:43:31 -0500

    Update CMake build scripts for unit testing

---
+++ QuickOpenBuildSettings.cmake
@@ -0,0 +1,19 @@
+macro(apply_QuickOpen_build_settings target_name)
+    find_package(wxWidgets REQUIRED core base)
+    include(${wxWidgets_USE_FILE})
+    target_include_directories(${target_name} PRIVATE ${wxWidgets_INCLUDE_DIRS})
+    target_link_libraries(${target_name} PRIVATE ${wxWidgets_LIBRARIES})
+
+    find_package(nlohmann_json CONFIG REQUIRED)
+    target_link_libraries(${target_name} PRIVATE nlohmann_json nlohmann_json::nlohmann_json)
+
+    find_package(civetweb REQUIRED)
+    target_link_libraries(${target_name} PRIVATE civetweb::civetweb civetweb::civetweb-cpp)
+
+    if(MSVC)
+        target_link_libraries(${target_name} PRIVATE "bcrypt.lib" "wbemuuid.lib")
+        set_property(TARGET ${target_name} PROPERTY VS_DPI_AWARE ON)
+    endif()
+
+    set_property(TARGET ${target_name} PROPERTY CXX_STANDARD 17)
+endmacro()
